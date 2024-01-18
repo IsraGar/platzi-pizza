dc1dc6de2964e375a00c1553818c44a95830b2dc
IsraGar <isragarciahdez@gmail.com>
2024-01-18 11:44:02 -0600

    Creando relaciones entre entidades

--- src/main/java/com/platzi/pizza/persistence/entity/OrderItemEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderItemEntity.java
@@ -24,4 +24,11 @@ public class OrderItemEntity {
     private Double quantity;
     @Column(nullable = false, columnDefinition = "DECIMAL(5,2)")
     private Double price;
+
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
+    private OrderEntity order;
 }
