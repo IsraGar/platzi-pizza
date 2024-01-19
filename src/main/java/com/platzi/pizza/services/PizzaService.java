e65d100422e0b8806eaa2dcad6b7df04a4aa231e
IsraGar <isragarciahdez@gmail.com>
2024-01-19 13:26:41 -0600

    Ordenando la información paginada

--- src/main/java/com/platzi/pizza/services/PizzaService.java
+++ src/main/java/com/platzi/pizza/services/PizzaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,9 +45,11 @@ public class PizzaService {
         this.pizzaRepository.deleteById(idPizza);
     }
 
-    public List<PizzaEntity> getAvailable(){
+    public Page<PizzaEntity> getAvailable(int pages, int elements, String sortBy, String sortDirection){
         System.out.println("********** "+this.pizzaRepository.countByVeganTrue());
-        return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageRequest = PageRequest.of(pages, elements, sort);
+        return this.pizzaPagSortRepository.findAllByAvailableTrue(pageRequest);
     }
 
     public PizzaEntity getByName(String name){
