521542135d993d0be0b5a80ddd7e27f7142a96c0
IsraGar <isragarciahdez@gmail.com>
2024-01-22 11:48:35 -0600

    Auditoría con Śpring Data

--- src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -4,13 +4,15 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity{
     @Id
     @Column(name = "id_pizza", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,4 +43,5 @@ public class PizzaEntity {
     public void setIdPizza(Integer idPizza) {
         this.idPizza = idPizza;
     }
+
 }
